4d4acd073486a3a78d1d58e3744a9591bc7aa92e
CHETHAN RAJ <CHETHANRAJ2001KN@GMAIL.COM>
2025-08-01 13:34:18 +0530

    Initial commit - Insure Me Spring Boot Project

---
+++ src/main/java/com/insureme/InsureMeApplication.java
@@ -0,0 +1,10 @@
+package com.insureme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsureMeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InsureMeApplication.class, args);
+    }
+}
---
+++ src/test/java/com/insureme/PolicyTests.java
@@ -0,0 +1,11 @@
+package com.insureme;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
